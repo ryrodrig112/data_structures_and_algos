7a1e730bfe7b91c54818b4aa0ea40d21b30d6c33
ryrodrig112 <ryrodrig112@gmail.com>
2022-06-17 15:53:30 -0700

    Begin writing test for max pairwise

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -8,6 +8,17 @@ def max_pairwise_product(numbers):
 
     return max_product
 
+def test_solution():
+    # Test Provided Solutions
+    assert max_pairwise_product([1, 2, 3]) == 3
+    assert max_pairwise_product([7, 5, 14, 2, 8, 8, 10, 1, 2, 3,]) == 140
+    # Test Large Numbers
+    assert max_pairwise_product([1000000,1000000]) == 1000000000000
+    # Test Time with large array
+    ...
+    # Create random test
+    ...
+
 
 if __name__ == '__main__':
     input_n = int(input())
