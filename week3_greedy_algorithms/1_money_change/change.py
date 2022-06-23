03154dbf2f09924182960934a6464d1d118667d4
ryrodrig112 <ryrodrig112@gmail.com>
2022-06-23 16:31:33 -0700

    Complete money change assignment

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -2,9 +2,17 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    num_10s = m // 10
+    num_5s = m % 10
+    num_1s = m % 5
+    return num_10s + num_5s + num_1s
+
+
+def test_algo():
+    assert get_change(2), "Given example, returned {}".format(get_change(2))
+    assert get_change(28), "Given example, returned {}".format(get_change(6))
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    test_algo()
+    # m = int(sys.stdin.read())
+    # print(get_change(m))
