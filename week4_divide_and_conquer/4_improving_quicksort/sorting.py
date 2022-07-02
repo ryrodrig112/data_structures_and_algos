b432ff92b66a6795ce54eb111e84724b5e49438e
ryrodrig112 <ryrodrig112@gmail.com>
2022-07-01 21:24:35 -0700

    Create test_partition

--- week4_divide_and_conquer/4_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/4_improving_quicksort/sorting.py
@@ -2,9 +2,23 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
+
+def partition3(a, l=None, r=None):
+    x = a[l]
+    j = l
+    j_len = 1
+    for i in range(l+1, r+1):
+        tmp = a[i]
+        if a[i] < x:
+            a.delete(i)
+            a.insert(0, tmp)
+            j += 1
+        elif a[i] == x:
+            j_len += 1
+        else:
+            pass
+    return j, j_len
+
 
 def partition2(a, l, r):
     x = a[l]
@@ -16,7 +30,6 @@ def partition2(a, l, r):
     a[l], a[j] = a[j], a[l]
     return j
 
-
 def randomized_quick_sort(a, l, r):
     if l >= r:
         return
@@ -27,7 +40,14 @@ def randomized_quick_sort(a, l, r):
     randomized_quick_sort(a, l, m - 1);
     randomized_quick_sort(a, m + 1, r);
 
-def test_algo():
+
+def test_partition():
+    j, j_len = partition3([2, 2, 2, 3, 5, 1, 3, 0])
+    assert j == 2, j
+    assert j_len == 3, j_len
+
+
+def test_qs():
     qs_test = randomized_quick_sort([2, 3, 9, 2, 2], 0, 4)
     assert  qs_test== [2,2,2,3, 9], qs_test
 
