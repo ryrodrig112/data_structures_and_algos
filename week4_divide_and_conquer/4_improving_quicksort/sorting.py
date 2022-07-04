d988e04a86855333598b8839f78107f54f72723c
ryrodrig112 <ryrodrig112@gmail.com>
2022-07-04 15:47:59 -0700

    Implement merge sort

--- week4_divide_and_conquer/4_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/4_improving_quicksort/sorting.py
@@ -8,39 +8,53 @@ def partition3(a, l, r):
     j = l
     j_len = 1
     for i in range(l+1, r+1):
-        tmp = a[i]
         if a[i] < x:
-            a.pop(i)
-            a.insert(0, tmp)
+            # remove a[i] from current location and put at beginning
+            # increment j
+            tmp = a[i]
+            del a[i]
+            a.insert(l, tmp)
             j += 1
+            print(a)
         elif a[i] == x:
+            # remove item from current location and put it at jth location
+            tmp = a[i]
+            del a[i]
+            a.insert(j, tmp)
             j_len += 1
+            print(a)
         else:
             pass
+    # a.insert(j, a[l])
     j_end = j + j_len
+    print(a)
+    print(a[j:j_end])
     return j, j_end
 
 
-def partition2(a, l, r):
-    x = a[l]  # pivot value
+def partition2(nums, l, r):
+    x = nums[l]  # pivot value
     j = l  # initial pivot location
     for i in range(l + 1, r + 1):
-        if a[i] <= x:
+        if nums[i] <= x:
             j += 1  # move pivot to the right
-            a[i], a[j] = a[j], a[i]  # swap ith element with pivot
-    a[l], a[j] = a[j], a[l] # put a [l] into the pivot location
+            nums[i], nums[j] = nums[j], nums[i]  # swap ith element with pivot
+        print(nums[i], nums)
+    nums[l], nums[j] = nums[j], nums[l] # put a [l] into the pivot location
+    print(nums)
     return j
+
 
 def randomized_quick_sort2(a, l, r):
     print("a: {}, l: {}, r: {}".format(a, l, r))
-    if l >= r :  # null
+    if l >= r:  # null
         return
     k = random.randint(l, r)  # pick a random index to be the pivot
     a[l], a[k] = a[k], a[l]  # switch a[l] and a[k] bc l is the pivot in partitions
     #use partition3
-    print("k = {}, a = {}".format(k, a))
+    print("k = {}, a = {}, r = {}".format(k, a, r))
     j = partition2(a, l, r)  # find the partition point for the random index
-    print("k = {}, m = {}, a: {}".format(k, j, a))
+    print("k = {}, m = {}, a: {}, r: {}".format(k, j, a, r))
     print("Left Half: {}, l: {}, r: {}".format(a[l:j], l, j))
     print("Right Half: {}, l: {}, r: {}".format(a[j+1:r+1], j+1, r+1))
     print("")
@@ -71,24 +85,25 @@ def randomized_quick_sort3(a, l, r):
 
 
 def test_partition():
-    j3, j_end = partition3([2, 2, 2, 3, 5, 1, 3, 0], l=0, r=7 )
-    assert j3 == 2, j3
-    assert j_end == 5, j_end
+    # j3, j_end = partition3([2, 2, 2, 3, 5, 1, 3, 0], l=0, r=7 )
+    # assert j3 == 2, j3
+    # assert j_end == 5, j_end
+
+    partition3([2, 2, 2, 3, 9], 0, 4)
 
     # j2 = partition2([2, 2, 2, 3, 5, 1, 3, 0],l=0,r=7)
     # assert  j2 == 4, j2
-    # assert a2 == [0, 2, 2, 1, 2, 3, 3, 5], a2
 
 
 def test_qs():
-    qs_test = randomized_quick_sort2([19, 20, 10, 30], 0, 3)
-    assert qs_test == [10, 19, 20, 30], qs_test
+    # qs_test = randomized_quick_sort2([19, 20, 10, 30], 0, 3)
+    # assert qs_test == [10, 19, 20, 30], qs_test
+    #
+    # qs_test2 = randomized_quick_sort2([2, 3, 9, 2, 2], 0, 4)
+    # assert qs_test2 == [2, 2, 2, 3, 9], qs_test2
 
-    qs_test2 = randomized_quick_sort2([2, 3, 9, 2, 2], 0, 4)
-    assert qs_test2 == [2, 2, 2, 3, 9], qs_test2
-
-    # qs_test3 = randomized_quick_sort3([2, 3, 9, 2, 2], 0, 4)
-    # assert qs_test3 == [2, 2, 2, 3, 9], qs_test3
+    qs_test3 = randomized_quick_sort3([2, 3, 9, 2, 2], 0, 4)
+    assert qs_test3 == [2, 2, 2, 3, 9], qs_test3
 
 
 
@@ -98,5 +113,5 @@ if __name__ == '__main__':
     # randomized_quick_sort(a, 0, n - 1)
     # for x in a:
     #     print(x, end=' ')
-    test_partition()
+    # test_partition()
     test_qs()
--- week4_divide_and_conquer/5_number_of_inversions/inversions.py
+++ week4_divide_and_conquer/5_number_of_inversions/inversions.py
@@ -1,17 +1,61 @@
 import sys
 
+
+def merge(b, c):
+    print("b: {}, c: {}".format(b,c))
+    d = []
+    while b and c:
+        print(b, c)
+        b1 = b[0]
+        c1 = c[0]
+        if b1 <= c1:
+            d.append(b1)
+            b.pop(0)
+        else:
+            d.append(c1)
+            c.pop(0)
+    if b:
+        d += b
+    elif c:
+        d += c
+    print("d: {}".format(d))
+    return d
+
+
 def get_number_of_inversions(a, b, left, right):
     number_of_inversions = 0
     if right - left <= 1:
-        return number_of_inversions
+        return a
     ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+    b = get_number_of_inversions(a, b, left, ave)
+    c = get_number_of_inversions(a, b, ave, right)
+    d = merge(b, c)
+    return d
+
+
+def merge_sort(a):
+    print("a: {}".format(a))
+    if len(a) <= 1:
+        return a
+    m = len(a)//2
+    b = merge_sort(a[0:m])
+    c = merge_sort(a[m:])
+    a_prime = merge(b, c)
+    return a_prime
+
+
+def test_algo():
+
+    # assert merge_sort([1, 3, 2, 4], [0, 0, 0, 0], 0, 4) == [1, 2, 3, 4],\
+    #     merge_sort([1, 3, 2, 4], [0], 0, 4)
+    assert merge_sort([1,3,2,4]) == [1,2,3,4], merge_sort([1,3,2,4])
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    # input = sys.stdin.read()
+    # n, *a = list(map(int, input.split()))
+    # print(n)
+    # b = n * [0]
+    # print(b)
+    # print(get_number_of_inversions(a, b, 0, len(a)))
+    test_algo()
