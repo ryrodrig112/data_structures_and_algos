3d9fa4f3485062ba58d7a9a281659e8f54bb0385
ryrodrig112 <ryrodrig112@gmail.com>
2022-07-09 15:58:05 -0700

    Complete lcs2 and lcs3

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -3,20 +3,51 @@
 import sys
 
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+    #0. fix 0's to the start of each string
+    #1. Create A starting with all zeros
+    #2. iterate through A by taking the max of the values to the left unless a[i] == b[j] then +1
+
+    a.insert(0,None)
+    b.insert(0, None)
+    A = [[0 for i in range(len(b))] for j in range(len(a))]
+
+    for j in range(len(a)):
+        for i in range(len(b)):
+            if i > 0:
+                left_case = A[j][i-1]
+                above_case = A[j-1][i]
+                diagonal_case = A[j-1][i-1]
+
+                if a[j] == b[i]:
+                    fill = max([left_case, above_case, diagonal_case]) + 1
+                else:
+                    fill = max([left_case, above_case, diagonal_case])
+                A[j][i] = fill
+    print(a,b, A[-1][-1])
+    return A[-1][-1]
+
+def test_algo():
+    tc1 = lcs2([1,2,3], [1,2,3,4,])
+    assert tc1 == 3, tc1
+
+    tc2 = lcs2([7], [1,2,3,4])
+    assert tc2 ==0, tc2
+
+    tc3 = lcs2([2,7,8,3], [5,2,8,7])
+    assert tc3 == 2, tc3
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-
-    n = data[0]
-    data = data[1:]
-    a = data[:n]
-
-    data = data[n:]
-    m = data[0]
-    data = data[1:]
-    b = data[:m]
-
-    print(lcs2(a, b))
+    # input = sys.stdin.read()
+    # data = list(map(int, input.split()))
+    #
+    # n = data[0]
+    # data = data[1:]
+    # a = data[:n]
+    #
+    # data = data[n:]
+    # m = data[0]
+    # data = data[1:]
+    # b = data[:m]
+    #
+    # print(lcs2(a, b))
+    test_algo()
--- week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -3,21 +3,57 @@
 import sys
 
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    # fix blanks to the front of a, b, c
+    # create 3d matrix a that is a tall, b wide, c deep
+    # iterate through but each spot has to equal eachother
+    for list in [a, b, c]:
+        list.insert(0, None)
+    # print("a: {}, b: {}, c: {}". format(a, b, c))
+    height = len(a)
+    width = len(b)
+    depth = len(c)
+    A = [[[0 for x in range(width)] for y in range(height)] for z in range(depth)]  # create A
+    for z in range(depth): # iterate through A, starting at the front, and moving left, then down, then back
+        for y in range(height):
+            for x in range(width):
+                if x and y and z > 0:
+                    # 2d first, need left and right
+                    left_case = A[z][y][x-1]
+                    above_case = A[z][y-1][x]
+                    depth_case = A[z-1][y][x]
+                    options = [left_case, above_case, depth_case]
+                    if a[y] == b[x] == c[z]:
+                        fill = max(options) + 1
+                    else:
+                        fill = max(options)
+                    A[z][y][x] = fill
+    return A[-1][-1][-1]
+
+
+def test_algo():
+    tc1 = lcs3([1], [1,2], [3,1,2])
+    assert tc1 == 1, tc1
+
+    tc2 = lcs3([1, 2, 3], [2, 1, 3], [1, 3, 5])
+    assert tc2 == 2, tc2
+
+    tc3 = lcs3([8, 3, 2, 1, 7], [8, 2, 1, 3, 8, 10, 7], [6, 8, 3, 1, 4, 7])
+    assert tc3 == 3, tc3
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    an = data[0]
-    data = data[1:]
-    a = data[:an]
-    data = data[an:]
-    bn = data[0]
-    data = data[1:]
-    b = data[:bn]
-    data = data[bn:]
-    cn = data[0]
-    data = data[1:]
-    c = data[:cn]
-    print(lcs3(a, b, c))
+    # input = sys.stdin.read()
+    # data = list(map(int, input.split()))
+    # an = data[0]
+    # data = data[1:]
+    # a = data[:an]
+    # data = data[an:]
+    # bn = data[0]
+    # data = data[1:]
+    # b = data[:bn]
+    # data = data[bn:]
+    # cn = data[0]
+    # data = data[1:]
+    # c = data[:cn]
+    # print(lcs3(a, b, c))
+    test_algo()
